7804f30102e4974dc507e9b1379c621223b2962c
jeanbejar <jeanevabejar@gmail.com>
2024-01-31 20:44:51 +0800

    downcase isogram

--- isograms.rb
+++ isograms.rb
@@ -1,5 +1,5 @@
 def is_isogram(word)
-word_array = word.split("")
+word_array = word.downcase.split("")
 char_count = Set.new(word_array)
 isogram_word = char_count.to_a
 return isogram_word == word_array
