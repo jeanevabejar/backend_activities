c2157decce819112cbb101884ceae644e0c70d07
jeanbejar <jeanevabejar@gmail.com>
2024-02-01 20:52:38 +0800

    isogram tester

--- isograms.rb
+++ isograms.rb
@@ -4,3 +4,7 @@ char_count = Set.new(word_array)
 isogram_word = char_count.to_a
 return isogram_word == word_array
 end
+
+puts is_isogram("Dermatoglyphics");
+puts is_isogram("aba");
+puts is_isogram("moOse");
