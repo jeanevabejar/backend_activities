0b9a24e7f6829ddb5bb54601d4f36644aaade717
jeanbejar <jeanevabejar@gmail.com>
2024-02-01 15:05:12 +0800

    updating and checking working table

--- main.session.sql
+++ main.session.sql
@@ -40,30 +40,45 @@ SELECT * FROM students ORDER BY age DESC;
 
 SELECT * FROM students;
 
+INSERT INTO students (
+        id,
+        first_name,
+        last_name,
+        age,
+        location
+    )
+VALUES (6, 'Finn', 'Lam', 25, 'Manila');
 
-CREATE TABLE research_papers(
+
+CREATE TABLE research_papers (
     id INT PRIMARY KEY,
-    student_id INT, FOREIGN KEY (student_id) REFERENCES students(id)
-    grade VARCHAR(2)
+    student_id INT, 
+    grade VARCHAR(1) CHECK (grade IN ('A', 'B', 'C', 'D', 'E', 'F')),
+    FOREIGN KEY (student_id) REFERENCES students(id)
 );
+
+
+
 
 INSERT INTO research_papers (id, student_id, grade)
 VALUES
-(1, 1, B),
-(2, 1, A),
-(3, 2, A),
-(4, 3, C),
-(5, 4, null),
-(6, 3, D),
-(7, 5, E),
-(8, 6, null),
-(9, 7, E),
-(10, 8, B);
+(1, 1, 'B'),
+(2, 1, 'A'),
+(3, 2, 'A'),
+(4, 2, 'C'),    
+(5, 3, 'C'),
+(6, 3, 'D'),
+(7, 4, NULL),   
+(8, 5, 'E'),
+(9, 6, NULL),   
+(10, 6, 'A');
 
 SELECT s.first_name, s.last_name, COUNT(r.id) AS number_of_research_papers
 FROM students s
-JOIN research_papers 
-ON s.id = r.student_id;
+JOIN research_papers r ON s.id = r.student_id
+GROUP BY s.id, s.first_name, s.last_name
+HAVING COUNT(r.id) > 1;
+
 
 SELECT s.first_name, s.last_name, r.id, r.grade
 FROM students s
