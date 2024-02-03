3513ec1fcfc8999f158d50630e713fd019b1ee9e
jeanbejar <jeanevabejar@gmail.com>
2024-02-03 09:25:19 +0800

    disemvowel tester

--- disemvowel_trolls.rb
+++ disemvowel_trolls.rb
@@ -1,3 +1,5 @@
 def disemvowel_trolls(str)
-    str.split('').select { |char| !char.match(/^[aeiou]$/i) }.join('')
+    str.split('').select { |char| !char.match(/[aeiou]/i) }.join('')
 end
+
+puts disemvowel_trolls('This website is for losers LOL!')
