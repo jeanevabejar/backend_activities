f29a27ea73ee395022fa348e0d454bf64f6e255f
jeanbejar <jeanevabejar@gmail.com>
2024-02-20 19:45:20 +0800

    add the_vowel_code file

---
+++ the_vowel_code.rb
@@ -0,0 +1,13 @@
+
+
+def encode(str)
+    str.gsub(/[aeiou]/i, "a"=>"1", "e"=>"2", "i"=>"3", "o"=>"4", "u"=>"5")
+end
+
+def decode(str)
+    str.gsub(/[12345]/i, "1"=>"a", "2"=>"e", "3"=>"i", "4"=>"o", "5"=>"u")
+    
+end
+
+puts encode("hello")
+puts decode("hello")
